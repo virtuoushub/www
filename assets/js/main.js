4e68abc2ce8edd6a9022916dd89f820c4302eb80
iinfin <han@iinf.in>
2020-08-06 09:57:44 +0300

    test pwa

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,12 @@ console.log("⧜");
 
 window.onload = () => {
 	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker.register("/sw.js");
+		navigator.serviceWorker.register("/sw.js", { scope: "/" }).then(function (registration) {
+			console.log("Service Worker Registered");
+		});
+
+		navigator.serviceWorker.ready.then(function (registration) {
+			console.log("Service Worker Ready");
+		});
 	}
 };

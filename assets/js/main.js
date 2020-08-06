de0b0d228c0530c2673613cc259b4287c5f81222
iinfin <han@iinf.in>
2020-08-06 09:58:58 +0300

    remove pwa

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,15 +33,3 @@ function noise() {
 noise();
 
 console.log("⧜");
-
-window.onload = () => {
-	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker.register("/sw.js", { scope: "/" }).then(function (registration) {
-			console.log("Service Worker Registered");
-		});
-
-		navigator.serviceWorker.ready.then(function (registration) {
-			console.log("Service Worker Ready");
-		});
-	}
-};

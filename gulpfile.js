c8d9cd4ea7cb813c9c5d047595a3f08e5c8dd681
iinfin <han@iinf.in>
2020-08-09 10:46:40 +0300

    merge tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,34 @@ const gulp = require("gulp"),
 	shell = require("gulp-shell"),
 	workbox = require("workbox-build");
 
-gulp.task("worker", () => {
-	return workbox.generateSW({
-		cacheId: "u29dc",
-		globDirectory: "./public",
-		globIgnores: ["**/index.xml", "**/index.json", "**/sitemap.xml"],
-		globPatterns: ["**/*.{js,css,html}"],
-		navigateFallback: "/404.html",
-		sourcemap: false,
-		swDest: "./public/worker.js",
-		runtimeCaching: [
-			{
-				urlPattern: /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico|webmanifest)$/,
-				handler: "CacheFirst",
-			},
-			{
-				urlPattern: /\.(?:json|xml)$/,
-				handler: "NetworkOnly",
-			},
-		],
-	});
-});
+gulp.task(
+	"worker",
+	gulp.series(
+		function () {
+			return workbox.generateSW({
+				cacheId: "u29dc",
+				globDirectory: "./public",
+				globIgnores: ["**/index.xml", "**/index.json", "**/sitemap.xml"],
+				globPatterns: ["**/*.{js,css,html}"],
+				navigateFallback: "/404.html",
+				sourcemap: false,
+				swDest: "./public/worker.js",
+				runtimeCaching: [
+					{
+						urlPattern: /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico|webmanifest)$/,
+						handler: "CacheFirst",
+					},
+					{
+						urlPattern: /\.(?:json|xml)$/,
+						handler: "NetworkOnly",
+					},
+				],
+			});
+		},
+		function () {
+			return gulp.src("public/work*.js", { read: false }).pipe(shell(["terser <%= file.path %> -m -c -o <%= file.path %>"]));
+		}
+	)
+);
 
-gulp.task("terser", () => {
-	return gulp.src("public/work*.js", { read: false }).pipe(shell(["terser <%= file.path %> -m -c -o <%= file.path %>"]));
-});
-
-gulp.task("default", gulp.series("worker", "terser"));
+gulp.task("default", gulp.series("worker"));

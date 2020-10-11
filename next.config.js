7ff4926ee6c918289bc76a014ab73141c6aea838
iinfin <48106228+iinfin@users.noreply.github.com>
2020-10-11 10:31:29 +0300

    use next-offline defaults

--- next.config.js
+++ next.config.js
@@ -1,26 +1,8 @@
 const withOffline = require('next-offline');
 
 const nextConfig = {
-	generateInDevMode: true,
 	workboxOpts: {
 		swDest: 'static/service-worker.js',
-		runtimeCaching: [
-			{
-				urlPattern: /^https?.*/,
-				handler: 'NetworkFirst',
-				options: {
-					cacheName: 'https-calls',
-					networkTimeoutSeconds: 15,
-					expiration: {
-						maxEntries: 150,
-						maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-					},
-					cacheableResponse: {
-						statuses: [0, 200],
-					},
-				},
-			},
-		],
 	},
 };
 
